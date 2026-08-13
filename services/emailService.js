c3b3721ddb76001960e06993e3a5cc5d8fe030b4
Pawan Rajput <rajputpawan824@gmail.com>
2026-08-13 11:31:07 +0530

    changes

--- services/emailService.js
+++ services/emailService.js
@@ -2,7 +2,7 @@
 import nodemailer from "nodemailer";
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url";  
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
